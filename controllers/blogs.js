567f8f3bb92570c631541d99e0d83c628dc2b86b
jivan85 <jivan0608@gmail.com>
2024-08-24 13:00:39 +0300

    uibuild

---
+++ controllers/blogs.js
@@ -0,0 +1,88 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response, next) => {
+  const blog = await Blog.findById(request.params.id)
+  try {
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.post('/', async (request, response, next) => {
+  const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)  
+  if (!decodedToken.id) {    
+    return response.status(401).json({ error: 'token invalid' })  
+  }  
+  const user = await User.findById(decodedToken.id)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id
+  })
+  try {
+    const savedBlog = await blog.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+    response.json(savedBlog)
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+  }
+  catch (error) {
+    next(error)
+  }
+})
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+module.exports = blogsRouter
---
+++ tests/blog_api.test.js
@@ -0,0 +1,106 @@
+const helper = require('./test_helper') 
+const { test, after, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const Blog = require('../models/blog')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+
+
+const initialBlogs = [
+    {
+        title: 'HTML is easy',
+        author: 'Lilly Pop',
+        url: 'www.lilly.com',
+        likes: 5
+    },
+    {
+        title: 'JS is easy',
+        author: 'Dave Pop',
+        url: 'www.dave.com',
+        likes: 12
+    },
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+  
+    assert.strictEqual(response.body.length, initialBlogs.length)
+  })
+
+  describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+      await User.deleteMany({})
+  
+      const passwordHash = await bcrypt.hash('sekret', 10)
+      const user = new User({ username: 'root', passwordHash })
+  
+      await user.save()
+    })
+  
+    test('creation succeeds with a fresh username', async () => {
+      const usersAtStart = await helper.usersInDb()
+  
+      const newUser = {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'salainen',
+      }
+  
+      await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+      const usersAtEnd = await helper.usersInDb()
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+  
+      const usernames = usersAtEnd.map(u => u.username)
+      assert(usernames.includes(newUser.username))
+    })
+  
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+      const usersAtStart = await helper.usersInDb()
+  
+      const newUser = {
+        username: 'root',
+        name: 'Superuser',
+        password: 'salainen',
+      }
+  
+      const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+  
+      const usersAtEnd = await helper.usersInDb()
+      assert(result.body.error.includes('expected `username` to be unique'))
+  
+      assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+  })
+  
+
+after(async () => {
+    await mongoose.connection.close()
+})
